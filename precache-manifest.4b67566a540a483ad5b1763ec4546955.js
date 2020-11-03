42e01a863e49de3ee991f6419c04e4ab51a18549
Artem Kolinko <kalinrt@gmail.com>
2020-11-03 14:45:43 +0200

    Updates

---
+++ precache-manifest.4b67566a540a483ad5b1763ec4546955.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2bb9d629f3203645f3689b25666273",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "9b1d5bd893a855b1ec62",
+    "url": "/react-todo-list/static/js/2.28c70b6d.chunk.js"
+  },
+  {
+    "revision": "e3473fcca4bb6ec6f378",
+    "url": "/react-todo-list/static/js/main.a76ffb45.chunk.js"
+  },
+  {
+    "revision": "97eeeb4fb9d50e47ffa9",
+    "url": "/react-todo-list/static/js/runtime-main.4c232ffb.js"
+  }
+]);
